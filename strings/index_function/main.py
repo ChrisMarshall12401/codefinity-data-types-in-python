df015ac978b5c63dc47fa60ee3ef4900c6c8c536
ChrisMarshall12401 <>
2026-09-05 23:09:29 +0000

    index() Function

---
+++ strings/index_function/main.py
@@ -0,0 +1,14 @@
+string = "It is cool to have a burning desire for studying"
+
+# Find the index of "burning" word
+burning = string.index('burning')
+
+# Find the index of "desire" word
+desire = string.find("desire")
+
+# Find the index of "studying" word
+studying = string.find("studying")
+
+print("The index of the word burning is:", burning)
+print("The index of the word desire is:", desire)
+print("The index of the word studying is:", studying)
